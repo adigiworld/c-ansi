6af920b33c1db97b04f3ec9c4be53fd100ca05ed
Aadil Rasheed <asadigiworld@gmail.com>
2025-07-04 21:38:34 -0700

    Start: ANSI C

---
+++ src/01-Overview-of-C/1.3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+  printf("I see, \nI remember !\n");
+  return 0;
+}
---
+++ src/setup.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+int main(){
+	write(1,"You'r all setup!\n",17);
+	return 0;
+}
+
+
